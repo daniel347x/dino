f40e8a5585f8202a9fe038483a20948c2e8b8694
danissen <danissen@microsoft.com>
2021-05-20 14:29:06 -0400

    Ditto

--- vision_transformer.py
+++ vision_transformer.py
@@ -192,28 +192,28 @@ class _SegMap_TransConv(nn.Module):
         self.bridge2 = Mlp(embed_dim*4, hidden_features=embed_dim*4, out_features=start_channels, act_layer=nn.GELU, drop=0.1)
         out_size = 1
         out_size *= 2
-        self.conv1 = _conv_trans_block(start_channels, start_channels / out_size, nn.ReLU(), kernel_size=2, stride=2, padding=0) # 2x2
+        self.conv1 = _conv_trans_block(start_channels, start_channels // out_size, nn.ReLU(), kernel_size=2, stride=2, padding=0) # 2x2
         if out_size < patch_size:
             out_size *= 2
-            self.conv2 = _conv_trans_block(start_channels, start_channels / out_size, nn.ReLU(), kernel_size=2, stride=2, padding=0) # 4x4
+            self.conv2 = _conv_trans_block(start_channels, start_channels // out_size, nn.ReLU(), kernel_size=2, stride=2, padding=0) # 4x4
         else:
             self.conv2 = None
         if out_size < patch_size:
             out_size *= 2
-            self.conv3 = _conv_trans_block(start_channels, start_channels / out_size, nn.ReLU(), kernel_size=2, stride=2, padding=0) # 8x8
+            self.conv3 = _conv_trans_block(start_channels, start_channels // out_size, nn.ReLU(), kernel_size=2, stride=2, padding=0) # 8x8
         else:
             self.conv3 = None
         if out_size < patch_size:
             out_size *= 2
-            self.conv4 = _conv_trans_block(start_channels, start_channels / out_size, nn.ReLU(), kernel_size=2, stride=2, padding=0) # 16x16
+            self.conv4 = _conv_trans_block(start_channels, start_channels // out_size, nn.ReLU(), kernel_size=2, stride=2, padding=0) # 16x16
         else:
             self.conv4 = None
         if out_size < patch_size:
             out_size *= 2
-            self.conv5 = _conv_trans_block(start_channels, start_channels / out_size, nn.ReLU(), kernel_size=2, stride=2, padding=0) # 32x32
+            self.conv5 = _conv_trans_block(start_channels, start_channels // out_size, nn.ReLU(), kernel_size=2, stride=2, padding=0) # 32x32
         else:
             self.conv5 = None
-        self.conv_final = _conv_block_2(start_channels / out_size, 1, nn.ReLU(), 1, 1) # Drop down to 1 channel
+        self.conv_final = _conv_block_2(start_channels // out_size, 1, nn.ReLU(), 1, 1) # Drop down to 1 channel
 
     def forward(self, x):
         bridge = self.bridge1(x)
