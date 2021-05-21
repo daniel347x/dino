ac9cdf4c9429789dc6c8fd79150f9e77582d3333
danissen <danissen@microsoft.com>
2021-05-21 01:45:18 -0400

    Dot the i's

--- utils.py
+++ utils.py
@@ -198,7 +198,7 @@ class SmoothedValue(object):
 
     def __init__(self, window_size=20, fmt=None):
         if fmt is None:
-            fmt = "{value:.6f} ({median:.4f}, {global_avg:.4f})"
+            fmt = "{value:.8f} ({median:.4f}, {global_avg:.4f})"
         self.deque = deque(maxlen=window_size)
         self.total = 0.0
         self.count = 0
