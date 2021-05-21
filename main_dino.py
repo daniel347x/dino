3bc0d6503933bacc11b1d217b3dfa2de0ec853a8
danissen <danissen@microsoft.com>
2021-05-21 00:37:47 -0400

    Ditto

--- main_dino.py
+++ main_dino.py
@@ -372,23 +372,13 @@ def train_one_epoch(student, teacher, teacher_without_ddp, dino_loss, data_loade
             # convert inputs to PIL RGB image in BW
             # Collator is smart enough to return an extra dimension BEFORE the batch dimension when the dataset returns a list of items for one sample
 
-            # images:
-            # for each of IMAGES, SEGMAPS, and WEIGHTS:
+            # For each of IMAGES, SEGMAPS, and WEIGHTS:
             # DATALOADER returns a LIST of batch items of length BS,
             # and DATASET returns a LIST of tensors for each batch item of length NCROPS,
-            # and DATALOADER is smart enough to combine this LIST of TENSORS returned by the dataset
-            # in conjunction with the LIST of BATCH ITEMS it returns,
-            # into a LIST of BATCH ITEMS where each BATCH ITEM is a TENSOR of LENGTH
-            # corresponding to the LENGTH of the LIST returned for each BATCH ITEM by the DATASET.
-            # by CONVERTING it to a SINGLE TENSOR whose size is the product BS * NCROPS, [BS-ncrop-1, BS-ncrop-2, ...]
-            # so images is a LIST OF LISTS:
-            # The first list has length ncrops.
-            # Each list element has length batch size.
-            # Each batch item is a tensor.
-            # The same applies to segmaps and weights.
-            # (boxes has not been processed properly anywhere and is ignored)
-            # Ignoring whether list or tensor, the shape of images, etc. is:
-            # ncrops, batch size, channels, image height, image width
+            # which the DATALOADER converts into a TENSOR of that same length
+            # (boxes has not been processed properly anywhere and is ignored).
+            # Ignoring whether list or tensor, the shape of images, etc. is therefore:
+            # batch size, ncrops, channels, image height, image width
             # (channels is 3 for images, 4 for segmaps (one per segmentation class, inc. none), and 1 for weights)
             images, segmaps, weights, boxes = data
 
@@ -399,6 +389,9 @@ def train_one_epoch(student, teacher, teacher_without_ddp, dino_loss, data_loade
             print(f'type(images[0]): {type(images[0])}')
             print(f'type(segmaps[0]): {type(segmaps[0])}')
             print(f'type(weights[0]): {type(weights[0])}')
+            print(f'len(images): {len(images)}')
+            print(f'len(segmaps): {len(segmaps)}')
+            print(f'len(weights): {len(weights)}')
             print(f'len(images[0]): {len(images[0])}')
             print(f'len(segmaps[0]): {len(segmaps[0])}')
             print(f'len(weights[0]): {len(weights[0])}')
