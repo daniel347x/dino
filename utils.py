7aa0dde868e16889ef3fd806ca3cea6c4d8f2d84
danissen <danissen@microsoft.com>
2021-05-23 18:39:04 -0400

    Ditto

--- utils.py
+++ utils.py
@@ -425,15 +425,15 @@ def setup_for_distributed(is_master):
     """
     This function disables printing when not in master process
     """
-    import builtins as __builtin__
-    builtin_print = __builtin__.print
+    # import builtins as __builtin__
+    # builtin_print = __builtin__.print
 
-    def print(*args, **kwargs):
-        force = kwargs.pop('force', False)
-        if is_master or force:
-            builtin_print(*args, **kwargs)
+    # def print(*args, **kwargs):
+    #     force = kwargs.pop('force', False)
+    #     if is_master or force:
+    #         builtin_print(*args, **kwargs)
 
-    __builtin__.print = print
+    # __builtin__.print = print
 
 
 def init_distributed_mode(args):
